b2c7490d1d6a136402b46a01bbec36aefc6c21a6
PrincePrynx <poetykprynx@gmail.com>
2024-06-20 19:28:43 +0000

    Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,63 +1,46 @@
 #!/usr/bin/python3
 
+""" script that reads stdin line by line and computes metrics """
+
 import sys
-import signal
 
-# Initialize metrics
-total_size = 0
-status_code_counts = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
 
-def print_metrics():
-    """Prints the total file size and status code counts."""
-    print(f"File size: {total_size}")
-    for code in sorted(status_code_counts.keys()):
-        if status_code_counts[code] > 0:
-            print(f"{code}: {status_code_counts[code]}")
+def printStatus(dic, size):
+    """ Prints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
-def signal_handler(sig, frame):
-    """Handles keyboard interruption (CTRL + C) to print metrics."""
-    print_metrics()
-    sys.exit(0)
 
-signal.signal(signal.SIGINT, signal_handler)
+# sourcery skip: use-contextlib-suppress
+statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+               "404": 0, "405": 0, "500": 0}
 
-line_count = 0
+count = 0
+size = 0
 
-for line in sys.stdin:
-    try:
-        parts = line.split()
-        if len(parts) != 7:
-            continue
-        
-        ip_address = parts[0]
-        date = parts[3] + ' ' + parts[4]
-        request = parts[5] + ' ' + parts[6] + ' ' + parts[7]
-        status_code = parts[8]
-        file_size = parts[9]
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            printStatus(statusCodes, size)
 
-        if not status_code.isdigit() or not file_size.isdigit():
-            continue
+        stlist = line.split()
+        count += 1
 
-        file_size = int(file_size)
-        total_size += file_size
+        try:
+            size += int(stlist[-1])
+        except Exception:
+            pass
 
-        if status_code in status_code_counts:
-            status_code_counts[status_code] += 1
-        
-        line_count += 1
-        if line_count % 10 == 0:
-            print_metrics()
-    except Exception:
-        continue
+        try:
+            if stlist[-2] in statusCodes:
+                statusCodes[stlist[-2]] += 1
+        except Exception:
+            pass
+    printStatus(statusCodes, size)
 
-# Print metrics one last time if end of input is reached
-print_metrics()
+
+except KeyboardInterrupt:
+    printStatus(statusCodes, size)
+    raise
