cf00a279beefb83730f93d94fba16c2a8da306e8
PrincePrynx <poetykprynx@gmail.com>
2024-06-20 19:47:31 +0000

    LOG PARSING

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,57 +1,36 @@
 #!/usr/bin/python3
+"""Module performs a log parsing """
+
 import sys
-import signal
 
-# Dictionary to keep the count of status codes
-status_counts = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
-
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
-line_count = 0
-
-def print_statistics():
-    print("File size: {}".format(total_size))
-    for status in sorted(status_counts.keys()):
-        if status_counts[status] > 0:
-            print("{}: {}".format(status, status_counts[status]))
-
-def signal_handler(sig, frame):
-    print_statistics()
-    sys.exit(0)
-
-# Register the signal handler for keyboard interrupt
-signal.signal(signal.SIGINT, signal_handler)
+counter = 0
 
 try:
     for line in sys.stdin:
-        parts = line.split()
-        if len(parts) < 9:
-            continue
-        ip, _, _, date, method, url, protocol, status_code, file_size = parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], parts[7], parts[8]
-        
-        # Validate and process the extracted parts
-        if method != "\"GET" or url != "/projects/260" or protocol != "HTTP/1.1\"":
-            continue
-        try:
-            status_code = status_code.strip()
-            file_size = int(file_size.strip())
-            if status_code in status_counts:
-                status_counts[status_code] += 1
-                total_size += file_size
-                line_count += 1
-        except ValueError:
-            continue
-        
-        if line_count % 10 == 0:
-            print_statistics()
-except KeyboardInterrupt:
-    print_statistics()
-    sys.exit(0)
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
+
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
